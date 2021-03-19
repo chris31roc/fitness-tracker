e88887acf75b6d34176c2f3ae4a9404260b2538e
chris31roc <chris31roc@yahoo.com>
2021-03-18 23:21:31 -0400

    updated html-routes.js file in routes folder

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,17 +6,17 @@ module.exports = function(app) {
 
   // index route loads index page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   // exercise route loads exercise.html
   app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
 
   // stats route loads stats.html
   app.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 
 };
